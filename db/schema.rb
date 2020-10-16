4e20042bfc943648c47810bcead17f35c8d68ae5
DeVeres <vadimbirsan@gmail.com>
2020-10-16 18:07:11 +0300

    stage 17

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_090213) do
+ActiveRecord::Schema.define(version: 2020_10_16_142520) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "top_seats"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2020_10_16_090213) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -85,6 +84,8 @@ ActiveRecord::Schema.define(version: 2020_10_16_090213) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
